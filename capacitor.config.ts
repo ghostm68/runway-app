99346346783870e1bb965b6345b4c21ab0c1c38f
inky <42044862+ghostm68@users.noreply.github.com>
2025-10-25 09:13:38 +0000

    Add Android platform files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.inkrealm.runway',
+  appName: 'runway',
+  webDir: 'www'
+};
+
+export default config;
